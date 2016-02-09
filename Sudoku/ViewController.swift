45614a4cfee9107e6d980277f82de81864146ea3
lei xinye <xinyelei@lawn-128-61-35-245.lawn.gatech.edu>
2016-02-09 14:33:51 -0500

    Initial Commit

---
+++ Sudoku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sudoku
+//
+//  Created by xinye lei on 16/2/9.
+//  Copyright © 2016年 xinye lei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
